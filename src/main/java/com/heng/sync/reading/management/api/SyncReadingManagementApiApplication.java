e6fb6fa7e8e16f1414e4cf41d8755f0e996e8fcc
unknown <Shuheng-Zhang>
2021-11-06 18:55:43 +0800

    Init Project

---
+++ src/main/java/com/heng/sync/reading/management/api/SyncReadingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.heng.sync.reading.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncReadingManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncReadingManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heng/sync/reading/management/api/SyncReadingManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heng.sync.reading.management.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncReadingManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
