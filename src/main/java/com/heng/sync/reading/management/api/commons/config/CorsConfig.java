8a3e4a9042f17f422377a49b48a0d3cccac8878c
Shuheng-Zhang <zhangshuheng01@163.com>
2021-12-04 15:39:44 +0800

    新增 全局跨域配置

---
+++ src/main/java/com/heng/sync/reading/management/api/commons/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.heng.sync.reading.management.api.commons.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 全局 跨域(CORS) 配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
